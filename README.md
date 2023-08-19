9ae5977b20764bf6a961b99485e35675a38ed6f7
Jason Alexander <jasonalexander.dev@gmail.com>
2023-08-19 11:33:06 +0100

    Updated version number.

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ cargo add baby-emulator
 
 Cargo.toml:
 ```text
-baby-emulator = "32"
+baby-emulator = "0.1.4"
 ```
 
 ## Example 
--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@
 //! 
 //! Cargo.toml:
 //! ```text
-//! baby-emulator = "32"
+//! baby-emulator = "0.1.4"
 //! ```
 //! 
 //! ## Example 
