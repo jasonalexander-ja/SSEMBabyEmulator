dd1a8a7c8d1d736571c544e38089e983822950e7
Jason Alexander <jasonalexander.dev@gmail.com>
2023-09-17 17:43:11 +0100

    Fixed bug making parser exclude whitespaced asm lines

--- src/assembler/parser/tests/mod.rs
+++ src/assembler/parser/tests/mod.rs
@@ -13,6 +13,30 @@ fn test_strip_comments() {
     assert_eq!(strip_comments("jmp 0x3"), "jmp 0x3".to_owned());
 }
 
+const EXAMPLE_LINES: &str = 
+"
+ldn $start_value  ; Loads 10 into the accumulator 
+; Commented line
+:loop_start
+sub $subtract_val ; Subtract 1 from the accumulator 
+cmp               ; Skip the next jump instruction if the accumulator is negative 
+jmp $loop_start   ; Jump to the start of the loop 
+stp               ; Program stops when the accumulator is negative 
+    
+:subtract_val     ; Value to be subtracted
+abs 0d1
+ 
+:start_value      ; Value to start in the accumulator 
+abs 0d-10
+
+
+";
+
+#[test]
+fn test_split_filter_lines() {
+    assert_eq!(split_filter_lines(&EXAMPLE_LINES.to_owned()).len(), 10)
+}
+
 #[test]
 fn test_parse_tag() {
     assert_eq!(parse_tag("  test  ".to_owned()), Ok(LineType::Tag("test".to_owned())));
@@ -76,6 +100,24 @@ fn get_ogn_instructions() -> Vec<(String, Result<LineType, LineParseError>)> {
     ]
 }
 
+pub fn get_lines() -> Vec<(String, Result<LineType, LineParseError>)> {
+    let mut res = vec![
+        ("   abs   0xA   ".to_owned(), Ok(LineType::Absolute(Value::Value(10)))),
+        ("   :foo   ".to_owned(), Ok(LineType::Tag("foo".to_owned()))),
+    ];
+    res.append(&mut get_instructions());
+    res
+}
+
+pub fn get_lines_ogn() -> Vec<(String, Result<LineType, LineParseError>)> {
+    let mut res = vec![
+        ("   abs   0xA   ".to_owned(), Ok(LineType::Absolute(Value::Value(10)))),
+        ("   :foo   ".to_owned(), Ok(LineType::Tag("foo".to_owned()))),
+    ];
+    res.append(&mut get_ogn_instructions());
+    res
+}
+
 #[test]
 fn test_parse_absolute() {
     get_absolute().iter().for_each(|(v, r)| {
@@ -102,4 +144,58 @@ fn test_parse_instruction_ogn() {
     get_ogn_instructions().iter().for_each(|(v, r)| {
         assert_eq!(parse_instruction_ogn(v.to_string()), *r)
     });
+}
+
+#[test]
+fn test_parse_line() {
+    get_lines().iter().for_each(|(v, r)| {
+        assert_eq!(parse_line(v, false), *r);
+    });
+}
+
+#[test]
+fn test_parse_line_ogn() {
+    get_lines_ogn().iter().for_each(|(v, r)| {
+        assert_eq!(parse_line(v, true), *r);
+    });
+}
+
+#[test]
+fn test_parse_lines() {
+    let mut correct_lines: Vec<String> = get_instructions().iter()
+        .map(|(v, _)| v.clone())
+        .collect();
+    let incorrect_lines = correct_lines.clone();
+    correct_lines.pop();
+    match parse_lines(correct_lines.clone(), false) {
+        Ok(v) => assert_eq!(v.len(), correct_lines.len()),
+        Err(_) => panic!("Failed to parse correct lines. ")
+    };
+    let err_index = get_instructions().iter()
+        .position(|(_, v)| v.is_err())
+        .unwrap_or(0);
+    match parse_lines(incorrect_lines, false) {
+        Err((i, _)) => assert_eq!(i, err_index),
+        Ok(_) => panic!("Incorrect lines parsed correctly. ")
+    };
+}
+
+#[test]
+fn test_parse_lines_ogn() {
+    let mut correct_lines: Vec<String> = get_ogn_instructions().iter()
+        .map(|(v, _)| v.clone())
+        .collect();
+    let incorrect_lines = correct_lines.clone();
+    correct_lines.pop();
+    match parse_lines(correct_lines.clone(), true) {
+        Ok(v) => assert_eq!(v.len(), correct_lines.len()),
+        Err(_) => panic!("Failed to parse correct og notation lines. ")
+    };
+    let err_index = get_ogn_instructions().iter()
+        .position(|(_, v)| v.is_err())
+        .unwrap_or(0);
+    match parse_lines(incorrect_lines, true) {
+        Err((i, _)) => assert_eq!(i, err_index),
+        Ok(_) => panic!("Incorrect og notation lines parsed correctly. ")
+    };
 }
